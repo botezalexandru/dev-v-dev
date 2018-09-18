a8d6221327d01ea5c4fc38f4bffbb950007fdb20
Pafcal <a.pavaloi@gmail.com>
2018-09-18 17:07:37 +0300

    Removed files for start

--- src/index.js
+++ src/index.js
@@ -28,21 +28,7 @@ document.addEventListener("submit", function (e) {
     }
 });
 
-if ('serviceWorker' in navigator) {
-    setupPWA();
-}
-
-/** PWA */
-function setupPWA() {
-    const notificationEl = document.querySelector("header button");
-
-    navigator.serviceWorker.register("service-worker.js").then(swRegistration => {
-        console.log("Service worker succesfully registered!");
-        setupPushNotifications(swRegistration);
-    }, err => {
-        console.error(`SW installation failed: ${err}`);
-    });
-
+(function offlineUIIife() {
     let previousResults;
     let controlsEl = document.querySelector(".controls");
 
@@ -55,54 +41,18 @@ function setupPWA() {
         previousResults.remove();
     });
     window.addEventListener("offline", onOffline);
-
+    
     function onOffline() {
         previousResults = PreviousResultsUI(Model);
         controlsEl.classList.add("hidden");
         resultsEl.classList.add("hidden");
         mainEl.appendChild(previousResults);
     }
+}); 
 
-    function setupPushNotifications(registration) {
-        if (Notification.permission === "denied") {
-            return;
-        }
 
-        firebase.initializeApp(APP_CONFIG.FIREBASE);
-        const messaging = firebase.messaging();
-        messaging.useServiceWorker(registration);
-        messaging.usePublicVapidKey("BKVNAss4X_lajWgfZfs0hle2Rk_2UQQ0UmvBArseNaauN4M9jGOvvBOQytCX-rCB51XnsMtXipd_fSmAH93nSm4");
 
-        if (Notification.permission === "default") {
-            notificationEl.classList.remove("hidden");
-            notificationEl.addEventListener("click", () => {
-                Notification.requestPermission().then(permission => {
-                    if (permission === 'granted') {
-                        sendTokenToServer(messaging);
-                    } else if (permission === "denied") {
-                        notificationEl.remove();
-                    }
-
-                });
-            })
-        } else {
-            sendTokenToServer(messaging);
-        }
-
-    }
-
-    function sendTokenToServer(messaging) {
-        messaging.getToken().then(token => {
-            console.log(token);
-
-            firebase
-                .database()
-                .ref(`tokens/${token}`)
-                .set(true);
-
-            notificationEl.remove();
-        }, err => {
-            console.log(err);
-        });
-    }
+/** PWA */
+function setupPWA() {
+    // messaging.usePublicVapidKey("BKVNAss4X_lajWgfZfs0hle2Rk_2UQQ0UmvBArseNaauN4M9jGOvvBOQytCX-rCB51XnsMtXipd_fSmAH93nSm4");
 }
